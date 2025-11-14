328a9d10f1b8c4aa1911d77282b1bcb2be394676
Musician <---------- ---------- ---------- ---------- ---------->
2025-11-14 21:48:01 +0300

    Исходный код задачи 1.

---
+++ dz_2_1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <clocale>
+
+int main(void)
+{
+    setlocale(LC_ALL, "Russian");
+
+    double value = { 0.0 }; 
+    std::cout << "Введите число:" << std::endl;
+    std::cin >> value;
+    std::cout << "Введённое число:" << std::endl;
+    std::cout << value << "\n";
+
+    return 0;
+}
